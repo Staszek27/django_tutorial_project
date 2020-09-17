495b52d53b3d991b4bc531da5914a89d505391eb
Michał Bryjak <bryjak1133@gmail.com>
2020-09-17 03:20:18 +0200

    just keep going through tutorial

--- project/proj/pages/views.py
+++ project/proj/pages/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def homepage_view(request, *args, **kwargs):
     my_context = {
         'my_text' : 'this is about me',
-        'number' : 123
+        'number' : 123,
+        'my_list' : [1, 2, 3],
+        'magic_number' : 2
     }
     return render(request, 'home.html', my_context)
